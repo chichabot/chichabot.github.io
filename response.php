a779eb90503fc4a89ed67e84f6e3435962616bd8
chichabot <155241483+chichabot@users.noreply.github.com>
2023-12-30 22:01:06 +0800

    Add files via upload

---
+++ response.php
@@ -0,0 +1,20 @@
+<?php
+
+    $responses = [
+        "hi, ganda mo 105% !",
+        "bias kita sa buong mundo",
+        "tinapay mwehehe",
+        "i miss you kaya",
+        "hatdog sa champorado",
+        "sana makiss kita",
+        "sana mag picture tayo, mga hundred million million dollar dollar",
+        "cheescake k",
+        "ganda bangs mo beh",
+        "wla na ako mailagay"
+    ];
+
+    $randomResponse = $responses[array_rand($responses)];
+
+    echo $randomResponse;
+
+?>
